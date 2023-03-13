e551e70539dc021a5f8b5db99b353713a2b735fa
Ilyas <92800413+ilyasnawawie@users.noreply.github.com>
2023-03-13 16:52:04 +0800

    learned components using nextjs

---
+++ components/header.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface HeaderProps {
+  onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  logoSrc: string;
+  onLogout: () => void;
+}
+
+const logoSrc = "https://discord.com/https://media.discordapp.net/attachments/1084717973819899946/1084718017985904680/logo.png"
+
+export default function Header(props: HeaderProps) {
+  const { onSearch, logoSrc, onLogout } = props;
+
+  return (
+    <header className="bg-gray-50 flex justify-between items-center py-3 px-4">
+      <div className="md:flex-none">
+        <input type="text" placeholder="Search..." onChange={onSearch} />
+      </div>
+      <div className="md:flex-none">
+        <img src={logoSrc} alt="" />
+      </div>
+      <div className="flex items-center justify-center md:justify-end">
+        <div className="flex">
+          <button className="ml-4" onClick={onLogout}>
+            Logout
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
