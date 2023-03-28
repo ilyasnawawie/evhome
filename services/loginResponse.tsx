5c1f133e6c3a8cafb94e0988d8bf2406c3eeebd6
Ilyas <92800413+ilyasnawawie@users.noreply.github.com>
2023-03-28 15:55:33 +0800

    Update loginResponse.tsx

--- services/loginResponse.tsx
+++ services/loginResponse.tsx
@@ -7,7 +7,7 @@ interface Props {
 const LoginResponse = ({ success }: Props) => {
   return (
     <div className={`text-center mt-4 ${success ? 'text-green-600' : 'text-red-600'}`}>
-      {success ? 'sLogin successful!' : 'Login failed. Please try again.'}
+      {success ? 'Login successful!' : 'Login failed. Please try again.'}
     </div>
   );
 };
